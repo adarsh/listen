c3e43c391f02c9c385fa57e56f478051d8ac7d9f
Adarsh Pandit <adarshpandit@gmail.com>
2017-11-21 10:48:52 -0800

    Misc cleanup

--- app/services/notifier.rb
+++ app/services/notifier.rb
@@ -1,14 +1,8 @@
 # require 'slack-notifier'
 
 class Notifier
-  def initialize
-    @notifier = Slack::Notifier.new(ENV['SLACK_WEBHOOK_URL']) do
-      defaults(
-        channel: '#random',
-        username: 'notifier',
-      )
-    end
-  end
+  CHANNEL = '#random'
+  USERNAME = 'notifier'
 
   def send(message)
     notifier.ping(message)
@@ -16,5 +10,9 @@ class Notifier
 
   private
 
-  attr_reader :notifier
+  def notifier
+    @notifier ||= Slack::Notifier.new(ENV['SLACK_WEBHOOK_URL']) do
+      defaults(channel: CHANNEL, username: USERNAME)
+    end
+  end
 end
